f5d6b04c407efe1f99a14f5c2c2bab99c1be8dc8
qvortex-beem <ghujfeuhh1@gmail.com>
2026-02-07 02:52:16 +0300

    step 6: variables and data types in JavaScript

---
+++ main.js
@@ -0,0 +1,72 @@
+"use strict";
+let age = 20;
+let name = "Denis";
+let isStudent = true;
+
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Is student: ", isStudent);
+
+let value = 10;
+console.log(value);
+value = "Теперь это строка";
+console.log(value);
+value = true;
+console.log(value);
+
+let userName = "Алексей";
+console.log(`Привет, ${userName}!`);
+let price = 99.99;
+let temperature = -15;
+let infinity = 1 / 0;
+let nitANumber = 0 / 0;
+console.log(0.1 + 0.2);
+let bigNumber = 90n;
+let huge = BigInt("123123");
+let isAlive = true;
+let isWorking = false;
+let isAdult = age >= 18;
+let x;
+let y = undefined;
+let userData = null;
+let id = Symbol("id");
+let person = {
+   name: "Станислав",
+   age: 30,
+   isStudent: false,
+   sayHello: function () {
+      console.log("Привет!");
+   },
+};
+
+console.log(person.name);
+let fruits = ["яблоко", "банан", "апельсин"];
+let numbers = [1, 2, 3, 4, 5];
+let mixed = ["текст", 42, true, null];
+function sum(a, b) {
+   return a + b;
+}
+let multiply = function (x, y) {
+   return x * y;
+};
+console.log(sum(5, 3));
+
+let now = new Date();
+let birthday = new Date("1995-12-17");
+let a = 10;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(10 + "5");
+console.log("10" - 5);
+const numberArray = [1, 2, 3];
+numbersArray[0] = 10;
+console.log(numbersArray);
+numbersArray = [5, 6, 7];
+const persons = { name: "Denis", age: 18 };
+persons.age = 50;
+persons.city = "Volgograd";
+console.log(persons);
+person = { name: "Stas" };
